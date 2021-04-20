d85398ce2768632687d13ec237207a920c177349
Yuri Freire <yurifreireg@gmail.com>
2021-04-20 14:34:32 -0300

    Criei os repositórios, entidades, controllers e conexão com o banco de dados

---
+++ projeto_nlw/src/database/index.ts
@@ -0,0 +1,5 @@
+//Depois de instalado o typeorm, reflect-metadata, o sqlite3 e criado o arquivo ormconfig.json e passado o type 
+//nele
+//eu preciso criar a connection fazendo um import createConnection do typeorm
+import {createConnection} from "typeorm";
+createConnection();
--- projeto_nlw/src/server.ts
+++ projeto_nlw/src/server.ts
@@ -2,18 +2,18 @@ import express from "express"; //Precisar instalar as tipagens também com o com
 //Outra informação importante. É preciso instalar também o typescript como dependencia de desenvolvimento
 //Ele é utilizado apenas em ambientes de desenvolvimento (npm install typescript -D)
 //E em seguida dar um (tsc --init) para criar um tsconfig.json
-//Agora para que o node entenda as sintaxes que o node nao entende é preciso instaalr uma outra biblioteca
+//Agora para que o node entenda as sintaxes que o node nao entende é preciso instalar uma outra biblioteca
 //npm install ts-node-dev -D (também como dependencia de senvolvimento), depois disso add o script no package.json
 //{"dev": "ts-node-dev src/server.ts"}
 
+import { routes } from "./routes"
+
+import "./database"
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({ msg: "Olá, Mundo!" })
-})
+app.use(express.json())
 
-app.post("/", (req, res) => {
-    return res.json({ msg: "Usuário cadastrado com sucesso!" })
-})
+app.use(routes)
 
 app.listen(3333, () => console.log("Server is running on port 3333"));

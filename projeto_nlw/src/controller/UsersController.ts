63a867dc3f34a1b2623fcace27cd43fd9f857e70
Yuri Freire <yurifreireg@gmail.com>
2021-04-21 12:58:17 -0300

    criei a rota de post do usuário

--- projeto_nlw/src/controller/UsersController.ts
+++ projeto_nlw/src/controller/UsersController.ts
@@ -5,7 +5,7 @@ class UsersController {
     async create(req : Request, res: Response): Promise<Response>{
         const { email } = req.body
         const usersService = new UsersService()
-        const user = await usersService.create({ email })
+        const user = await usersService.create( email )
         return res.json( user )
     }
 }
--- projeto_nlw/src/routes.ts
+++ projeto_nlw/src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express"
 import {SettingsController} from "./controller/SettingsController"
+import { UsersController } from "./controller/UsersController";
 
 const routes = Router()
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
-routes.post('/settings', settingsController.create)
+routes.post('/settings', settingsController.create);
+routes.post('/users', usersController.create);
 
 export{routes};

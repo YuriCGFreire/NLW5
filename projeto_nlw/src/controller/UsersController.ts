43f8a016d5ddab89b338d225326646019fe30373
Yuri Freire <yurifreireg@gmail.com>
2021-05-13 14:24:48 -0300

    Update UsersController.ts

--- projeto_nlw/src/controller/UsersController.ts
+++ projeto_nlw/src/controller/UsersController.ts
@@ -3,10 +3,14 @@ import { UsersService } from "../services/UsersService"
 
 class UsersController {
     async create(req : Request, res: Response): Promise<Response>{
-        const { email } = req.body
-        const usersService = new UsersService()
-        const user = await usersService.create( email )
-        return res.json( user )
+        try{
+            const { email } = req.body
+            const usersService = new UsersService()
+            const user = await usersService.create( email )
+            return res.json( user )
+        }catch(err){
+            return res.json(err)
+        }
     }
 }
 
